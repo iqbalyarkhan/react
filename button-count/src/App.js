64000a5bc8241a1eb056d45bb920e44cac64d95b
iqbalyarkhan <iqbal.khan@utdallas.edu>
2019-12-14 16:17:17 -0600

    Adds comments

--- button-count/src/App.js
+++ button-count/src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 
 function App(){
   //This is not Javascript but JSX that is being
-  //converted to react API calls using Babel
+  //converted to react API calls using Babel that converts
+  //JSX to react API calls
   return <div>Hello React from App!</div>
 }
 
